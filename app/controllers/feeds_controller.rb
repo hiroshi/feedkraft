5ae79d3266da88df31ba1f907e515eb684f1c013
Hiroshi Saito <hiroshi3110@gmail.com>
2009-06-26 14:41:53 +0900

    Remove unused lines

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,42 +8,6 @@ class FeedsController < ApplicationController
     send_data @result_feed.to_s, :type => "text/xml; charset=UTF-8"
   end
 
-  def auth
-    require "openid"
-    require 'openid/store/filesystem'
-
-    store = OpenID::Store::Filesystem.new(Rails.root.join("db").join("cstore"))
-    consumer = OpenID::Consumer.new(session, store)
-    oidreq = consumer.begin("https://www.google.com/accounts/o8/id")
-    redirect_to oidreq.redirect_url(root_url, identify_session_url)
-  end
-
-  def complete
-    require "openid"
-    require 'openid/store/filesystem'
-
-    store = OpenID::Store::Filesystem.new(Rails.root.join("db").join("cstore"))
-    consumer = OpenID::Consumer.new(session, store)
-
-    oidresp = consumer.complete(params.reject{|k,v|request.path_parameters[k]}, complete_url)
-    case oidresp.status
-    when OpenID::Consumer::SUCCESS
-      flash[:success] = ("Verification of #{oidresp.display_identifier} succeeded.")
-      # TODO: store session
-    when OpenID::Consumer::FAILURE
-      if oidresp.display_identifier
-        flash[:error] = ("Verification of #{oidresp.display_identifier} failed: #{oidresp.message}")
-      else
-        flash[:error] = "Verification failed: #{oidresp.message}"
-      end
-    when OpenID::Consumer::SETUP_NEEDED
-      flash[:alert] = "Immediate request failed - Setup Needed"
-    when OpenID::Consumer::CANCEL
-      flash[:alert] = "OpenID transaction cancelled."
-    end
-    redirect_to root_path
-  end
-
   private
 
   def set_feeds
