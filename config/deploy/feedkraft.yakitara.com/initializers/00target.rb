16cd92145c1a88424e8c2018356c6accd2ff6491
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-03 16:17:43 +0900

    added uservoice code

--- config/deploy/feedkraft.yakitara.com/initializers/00target.rb
+++ config/deploy/feedkraft.yakitara.com/initializers/00target.rb
@@ -1,1 +1,11 @@
 FeedKraft[:google_analytics_id] = "UA-768998-4"
+FeedKraft[:user_voice_options] = {
+  :key => 'feedkraft',
+  :host => 'feedkraft.uservoice.com', 
+  :forum => 'general', 
+  :alignment => 'left',
+  :background_color =>'#f00', 
+  :text_color => 'white',
+  :hover_color => '#06C',
+  :lang => 'en',
+}
