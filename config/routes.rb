cc953977b1cc296b589873f6b4d1fcdd5d410386
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-06 14:31:54 +0900

    Added alternate route "/users/:user_id/filters/:id/feed" to keep track of subscription

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => "feeds"
   map.feed "feed", :controller => "feeds", :action => "feed"
   map.resources :filters, :member => {:feed => :get}, :collection => {:latest => :get}
+  map.feed_user_filter "/users/:user_id/filters/:id/feed", :controller => "filters", :action => "feed"
 
   map.resource :session, :only => [:new, :destroy], :member => {:inquire => :post, :identify => :get}
   map.resources :users
