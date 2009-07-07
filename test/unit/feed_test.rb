94b83f39b3759dc2db9aaba6ca5c7b08ed806182
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-07 22:56:43 +0900

    Fix unit tests

--- test/unit/feed_test.rb
+++ test/unit/feed_test.rb
@@ -4,21 +4,21 @@ class FeedTest < ActiveSupport::TestCase
   test "rss1 filter" do
     feed = Feed.parse(fixture_file_read("files/mycom.rss1"))
     assert_equal 50, feed.entries.size
-    feed.filter!(:identifier => "column", :category => "")
+    feed.filter!("@rdf:about" => "column")
     assert_equal 3, feed.entries.size
   end
 
   test "rss2 filter" do
     feed = Feed.parse(fixture_file_read("files/yakitara.rss2"))
     assert_equal 25, feed.entries.size
-    feed.filter!(:identifier => "", :category => "Rails")
+    feed.filter!(:category => "Rails")
     assert_equal 9, feed.entries.size
   end
 
   test "atom filter" do
     feed = Feed.parse(fixture_file_read("files/yakitara.atom"))
     assert_equal 25, feed.entries.size
-    feed.filter!(:identifier => "", :category => "Rails")
+    feed.filter!("category@term" => "Rails")
     assert_equal 9, feed.entries.size
   end
 end
