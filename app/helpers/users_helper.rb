69e3451b95925c205091e82a4be494d3afb72b29
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-07 16:32:18 +0900

    Implement user_name helper to urge fill user name

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_name(user)
+    if !user.name.blank?
+      h user.name
+    elsif current_user == user
+      link_to "no name", edit_user_path(user), :title => "Click to edit your name"
+    else
+      "no name"
+    end
+  end
 end
