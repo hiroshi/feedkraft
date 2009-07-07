fbcf30cfee41666848f759d6600b01636d61e51d
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-07 18:01:33 +0900

    Fix a problem, cannot boot in production

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+require "activesupport"
 module FeedKraft
   @@config = {
     # default values
