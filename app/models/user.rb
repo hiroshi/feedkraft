cd2b9efb8d99c2c3181ed0d8077ad34cf87ede4d
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-12 10:08:22 +0900

    Fix a defect, cannot create new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :filters
   has_many :subscriptions
 
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :allow_nil => true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "create multiple no name users" do
+    assert_equal 1, User.count(:conditions => 'name IS NULL')
+    User.create(:identity => "http://new.example.com")
+    assert_equal 2, User.count(:conditions => 'name IS NULL')
   end
 end
