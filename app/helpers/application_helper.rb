d6801e4a8c0fbb8e16e311e3d3a006c89a5a8bce
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-12 23:03:59 +0900

    Set HTML title from the subject feed even filter is not saved yet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def has_content_for?(name)
+    !instance_variable_get("@content_for_#{name}").nil?
+  end
 end
