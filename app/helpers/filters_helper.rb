6865379e67dfabbbf4222e087b8f8b7ffab6639f
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-13 14:07:15 +0900

    Refine text of filter key select options to be more human readable I wish

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -1,2 +1,21 @@
 module FiltersHelper
+  
+  def filter_key_options(feed, selected=nil)
+    entry_tag = feed.entries_xpath.split("/").last
+
+    options = ([selected] + feed.filter_fields.keys).compact.sort.map do |key|
+      case key
+      when /^@(.+)$/
+        [$1, key]
+      when /^[^@]+@/
+        tags, attr = key.split("@")
+        tags = tags.split("/")
+        last = tags.pop
+        [tags.map{|tag|"<#{tag}>"}.join + "<#{last} #{attr}>", key]
+      else
+        [key.split("/").map{|tag|"<#{tag}>"}.join, key]
+      end
+    end
+    grouped_options_for_select({"<#{entry_tag}>" => options}, selected)
+  end
 end
