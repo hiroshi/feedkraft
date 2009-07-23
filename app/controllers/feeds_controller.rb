73a47a3073468f855a36361f3ca7a8b3868495fb
Hiroshi Saito <hiroshi3110@gmail.com>
2009-07-23 18:29:22 +0900

    Fix #2 - The bookmarklet should be capable for paths in addition to URLs
    * Moved main script for bookmarklet as a file like google reader's

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,9 +1,16 @@
 class FeedsController < ApplicationController
 #  before_filter :set_feeds, :only => [:feed]
+  caches_page :bookmarklet
 
   def index
     @latest_filters = ::Filter.latest.with_subscription_key_for(current_user).all(:limit => 10)
     @popular_filters = ::Filter.popular(:with_subscription_key_for => current_user).all
+  end
+
+  def bookmarklet
+    respond_to do |format|
+      format.js
+    end
   end
 
 #   def feed
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => "feeds", :conditions => {:method => :get}
-#  map.feed "feed", :controller => "feeds", :action => "feed", :conditions => {:method => :get}
+  map.bookmarklet "bookmarklet.js", :controller => "feeds", :action => "bookmarklet", :conditions => {:method => :get}
+
   map.resources :filters, :member => {:feed => :get}, :collection => {:latest => :get}, :except => [:edit]
-#  map.feed_user_filter "users/:user_id/filters/:id/feed", :controller => "filters", :action => "feed", :conditions => {:method => :get}
 
   map.subscription "subscription/:key", :controller => "subscriptions", :action => "feed", :requirements => {:key => /[0-9a-f]{8}/i}, :conditions => {:method => :get}
   map.resources :subscriptions, :only => [:create]
