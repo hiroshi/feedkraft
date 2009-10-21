9b9f041fc965273e1dff558817c2f12d96a8543e
Hiroshi Saito <hiroshi3110@gmail.com>
2009-10-22 06:54:45 +0900

    Update for the replaced server

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,12 +5,13 @@ set :deploy_via, :remote_cache
 
 # USAGE: Use this task like:
 #   cap feedkraft.yakitara.com deploy
-desc "set target feedkraft.yakitara.com"
-task "feedkraft.yakitara.com" do
+desc "set target feedkraft.com"
+task "feedkraft.com" do
   set :target, task_call_frames.first.task.name
-  server "silent.yakitara.com", :app, :web, :db, :primary => true
+  server "tay.yakitara.com", :app, :web, :db, :primary => true
   set :deploy_to, "/var/www/feedkraft"
-  set :user, "www-data"
+#  set :user, "www-data"
+  set :group, "www-data"
   set :use_sudo, false
 end
 
@@ -19,3 +20,17 @@ task :copy_initializers do
   run "cp #{latest_release}/config/deploy/#{target}/initializers/*.rb #{latest_release}/config/initializers/ || exit 0"
 end
 after "deploy:update_code", "copy_initializers"
+
+namespace :deploy do
+  desc "change group of setuped directory"
+  task :setup_change_group do
+    run "chown -R :#{group} #{deploy_to}"
+  end
+  after "deploy:setup", "deploy:setup_change_group"
+
+  desc "change group of deployed directory"
+  task :change_group do
+    run "chown -R :#{group} #{latest_release}"
+  end
+  after "deploy:finalize_update", "deploy:change_group"
+end
