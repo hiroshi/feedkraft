29f3cacfb6491e002df27796955c37ebe8615e93
Hiroshi Saito <hiroshi3110@gmail.com>
2010-10-04 11:38:37 +0900

    Use oauth_url for oauth callback url to avoid "Twitter OAuth failed (401 Unauthorized)" caused by inconsistency of host between requested host and redirecting host.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def new
     config = Rails.application.config.oauth
     client = TwitterOAuth::Client.new(config.slice(:consumer_key, :consumer_secret))
-    request_token = client.request_token(config.slice(:oauth_callback))
+    request_token = client.request_token(:oauth_callback => oauth_url)
     session[:oauth_token] = request_token.token
     session[:oauth_secret] = request_token.secret
     session[:return_to] = params[:return_to]
+    logger.info "OAuth Authroize URL: #{request_token.authorize_url}"
     redirect_to request_token.authorize_url
   end
   
