8896cb88b5561af5dd732a6e7db9aa0520b0c147
Hiroshi Saito <hiroshi3110@gmail.com>
2011-04-24 22:01:30 +0900

    Update to deploy on jerle

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "feedkraft"
 set :repository,  "git://github.com/hiroshi/feedkraft.git"
+set :branch, "ruby19_rails3"
 set :scm, :git
 set :deploy_via, :remote_cache
 
@@ -15,9 +16,20 @@ task "feedkraft.com" do
   set :use_sudo, false
 end
 
+desc "set target feedkraft.yakitara.com"
+task "feedkraft.yakitara.com" do
+  set :target, task_call_frames.first.task.name
+  server "jerle.yakitara.com", :app, :web, :db, :primary => true
+  set :deploy_to, "/var/www/feedkraft"
+  set :user, "www-data"
+  set :group, "www-data"
+  set :use_sudo, false
+end
+
 desc "copy target initializers"
 task :copy_initializers do
   run "cp #{latest_release}/config/deploy/#{target}/initializers/*.rb #{latest_release}/config/initializers/ || exit 0"
+  run "cp #{shared_path}/config/initializers/*.rb #{latest_release}/config/initializers/ || exit 0"
 end
 after "deploy:update_code", "copy_initializers"
 
